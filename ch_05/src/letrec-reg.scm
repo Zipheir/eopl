8aab599dd7f3c41db3f3fce262c012913e1ef1de
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-04-29 16:20:25 -0400

    Stackify continuation representation.

--- ch_05/src/letrec-reg.scm
+++ ch_05/src/letrec-reg.scm
@@ -14,6 +14,14 @@
 (define *val-reg* 'uninitialized)
 (define *proc1-reg* 'uninitialized)
 
+;; cont-push! : Cont -> ()
+(define (cont-push! k)
+  (set! *cont-reg* (cons k *cont-reg*)))
+
+;; cont-drop! : () -> ()
+(define (cont-drop!)
+  (set! *cont-reg* (cdr *cont-reg*)))
+
 ;;;; Expressed values
 
 ;; expval->num : Exp-val -> Int
@@ -116,49 +124,52 @@
 ;;   *cont-reg* : Cont
 ;;   *val-reg*  : Exp-val
 ;;
-;; The continuation register is always updated first.
-(define (apply-cont)
-  (pmatch *cont-reg*
-    ((end-cont ,print-msg)
-     (when print-msg (display "End of computation.\n"))
-     *val-reg*)
-    ((zero1-cont ,k)
-     (set! *cont-reg* k)
+;; *cont-reg* is assumed to hold the rest of the continuation stack.
+(define (apply-cont cont)
+  (pmatch cont
+    (zero1-cont
      (set! *val-reg* `(bool-val ,(zero? (expval->num *val-reg*))))
-     (apply-cont))
-    ((if-test-cont ,exp2 ,exp3 ,env ,k)
-     (set! *cont-reg* k)
+     (continue))
+    ((if-test-cont ,exp2 ,exp3 ,env)
      (if (expval->bool *val-reg*)
          (set! *exp-reg* exp2)
          (set! *exp-reg* exp3))
      (set! *env-reg* env)
      (value-of/k))
-    ((let-exp-cont ,var ,body ,env ,k)
-     (set! *cont-reg* k)
+    ((let-exp-cont ,var ,body ,env)
      (set! *exp-reg* body)
      (set! *env-reg* (extend-env var *val-reg* env))
      (value-of/k))
-    ((diff1-cont ,exp2 ,env ,k)
-     (set! *cont-reg* `(diff2-cont ,*val-reg* ,k))
+    ((diff1-cont ,exp2 ,env)
+     (cont-push! `(diff2-cont ,*val-reg*))
      (set! *exp-reg* exp2)
      (set! *env-reg* env)
      (value-of/k))
-    ((diff2-cont ,val1 ,k)
-     (set! *cont-reg* k)
+    ((diff2-cont ,val1)
      (let ((num1 (expval->num val1))
            (num2 (expval->num *val-reg*)))
        (set! *val-reg* `(num-val ,(- num1 num2))))
-     (apply-cont))
-    ((rator-cont ,rand ,env ,k)
-     (set! *cont-reg* `(rand-cont ,*val-reg* ,env ,k))
+     (continue))
+    ((rator-cont ,rand ,env)
+     (cont-push! `(rand-cont ,*val-reg*))
      (set! *env-reg* env)
      (set! *exp-reg* rand)
      (value-of/k))
-    ((rand-cont ,vrat ,env ,k)
-     (set! *cont-reg* k)
+    ((rand-cont ,vrat)
      (set! *proc1-reg* (expval->proc vrat))
      (apply-procedure/k))
-    (? (error 'apply-cont "invalid continuation" *cont-reg*))))
+    (? (error 'apply-cont "invalid continuation" cont))))
+
+;; continue () -> Final-answer
+;;
+;; If the continuation stack is empty, halt.  Otherwise, pop the
+;; top continuation and apply it.
+(define (continue)
+  (if (null? *cont-reg*)
+      *val-reg*
+      (let ((k (car *cont-reg*)))
+        (cont-drop!)
+        (apply-cont k))))
 
 ;;;; Interpreter
 
@@ -166,7 +177,7 @@
 (define (value-of-program pgm print-msg)
   (pmatch pgm
     ((program ,exp1)
-     (set! *cont-reg* `(end-cont ,print-msg))
+     (set! *cont-reg* '())
      (set! *exp-reg* exp1)
      (set! *env-reg* (init-env))
      (value-of/k))))
@@ -178,29 +189,27 @@
   (pmatch *exp-reg*
     ((const-exp ,n)
      (set! *val-reg* `(num-val ,n))
-     (apply-cont))
+     (continue))
     ((var-exp ,var)
      (set! *val-reg* (apply-env *env-reg* var))
-     (apply-cont))
+     (continue))
     ((proc-exp ,var ,body)
      (set! *val-reg* `(proc-val ,(procedure var body *env-reg*)))
-     (apply-cont))
+     (continue))
     ((zero?-exp ,exp1)
-     (set! *cont-reg* `(zero1-cont ,*cont-reg*))
+     (cont-push! 'zero1-cont)
      (set! *exp-reg* exp1)
      (value-of/k))
     ((diff-exp ,exp1 ,exp2)
-     (set! *cont-reg* `(diff1-cont ,exp2 ,*env-reg* ,*cont-reg*))
+     (cont-push! `(diff1-cont ,exp2 ,*env-reg*))
      (set! *exp-reg* exp1)
      (value-of/k))
     ((if-exp ,exp1 ,exp2 ,exp3)
-     (set! *cont-reg*
-           `(if-test-cont ,exp2 ,exp3 ,*env-reg* ,*cont-reg*))
+     (cont-push! `(if-test-cont ,exp2 ,exp3 ,*env-reg*))
      (set! *exp-reg* exp1)
      (value-of/k))
     ((let-exp ,var ,exp1 ,body)
-     (set! *cont-reg*
-           `(let-exp-cont ,var ,body ,*env-reg* ,*cont-reg*))
+     (cont-push! `(let-exp-cont ,var ,body ,*env-reg*))
      (set! *exp-reg* exp1)
      (value-of/k))
     ((letrec-exp ,p-name ,b-var ,p-body ,lr-body)
@@ -208,7 +217,7 @@
      (set! *env-reg* (extend-env-rec p-name b-var p-body *env-reg*))
      (value-of/k))
     ((call-exp ,rator ,rand)
-     (set! *cont-reg* `(rator-cont ,rand ,*env-reg* ,*cont-reg*))
+     (cont-push! `(rator-cont ,rand ,*env-reg*))
      (set! *exp-reg* rator)
      (value-of/k))
     (? (error 'value-of/k "invalid expression" exp))))
