331604181c39bdc9897df4a2b550bb60ab1a68d2
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-05-02 20:23:58 -0400

    More tests.

--- ch_05/src/ada/run_tests.adb
+++ ch_05/src/ada/run_tests.adb
@@ -22,9 +22,31 @@ procedure Run_Tests is
   end Test_Num_Exp;
 
   E: Expr_Ptr;
+  Add1: Expr_Ptr;
 begin
+  -- Utility +1 procedure expression.
+  Add1 := new Expression'(Proc_Exp, 'A',
+            new Expression'(Diff_Exp,
+              new Expression'(Var_Exp, 'A'),
+              new Expression'(Const_Exp, -1)));
+
+  E := new Expression'(Const_Exp, 4);
+  Test_Num_Exp("constant", 4, E);
+
+  E := new Expression'(Var_Exp, 'X');
+  Test_Num_Exp("variable", 10, E);
+
+  E := new Expression'(Call_Exp, Add1,
+         new Expression'(Const_Exp, 4));
+  Test_Num_Exp("application", 5, E);
+
   E := new Expression'(Diff_Exp,
            new Expression'(Const_Exp, 5),
            new Expression'(Const_Exp, 2));
   Test_Num_Exp("diff constants", 3, E);
+
+  E := new Expression'(Diff_Exp,
+           new Expression'(Var_Exp, 'X'),
+           new Expression'(Var_Exp, 'I'));
+  Test_Num_Exp("diff variables", 9, E);
 end Run_Tests;
