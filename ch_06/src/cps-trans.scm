eaa346bc9181fe7b1b71be833ab42ca53cf9a891
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-05-12 16:31:39 -0400

    Add ->CPS translator. (Rough)

---
+++ ch_06/src/cps-trans.scm
@@ -0,0 +1,186 @@
+;;;; CPS-IN -> CPS-OUT translator from Ch. 6
+
+(import (rnrs base (6))
+        (rename (rnrs lists (6)) (for-all every))
+        )
+
+(include "../../src/pmatch.scm")
+
+;;; Utility
+
+;; list-index : List-of(A) x (A -> Bool) -> (Int + False)
+(define (list-index pred xs)
+  (letrec
+   ((index
+     (lambda (xs i)
+       (pmatch xs
+         (() #f)
+         ((,x . ,xs*)
+          (if (pred x)
+              i
+              (index xs* (+ i 1))))))))
+    (index xs 0)))
+
+;; list-set : List x Int x Scheme-val -> List
+(define (list-set xs k y)
+  (if (zero? k)
+      (cons y xs)
+      (pmatch xs
+        (() (error 'list-set "short list"))
+        ((,x . ,xs*)
+         (cons x (list-set xs* (- k 1) y))))))
+
+;;; Simple gensyms
+
+(define fresh-identifier
+  (let ((name-counter 0))
+    (lambda (prefix)
+      (let ((res (string->symbol
+                  (string-append (symbol->string prefix)
+           	                 (number->string name-counter)))))
+        (set! name-counter (+ name-counter 1))
+        res))))
+
+;; cps-of-exps : List-of(Inp-exp) x (List-of(Inp-exp) -> Tf-exp) ->
+;;		   Tf-exp
+;; Usage: CPSs a list of subexpressions.  The translated list is
+;; passed to builder.
+(define (cps-of-exps exps builder)
+  (letrec
+   ((cps-of-rest
+     (lambda (exps)
+       (let ((pos (list-index
+                   (lambda (exp)
+	             (not (inp-exp-simple? exp)))
+	           exps)))
+	 (if (not pos)
+	     (builder (map cps-of-simple-exp exps))
+	     (let ((var (fresh-identifier 'var)))
+	       (cps-of-exp (list-ref exps pos)
+	                   `(cps-proc-exp
+	                     ,(list var)
+	                     ,(cps-of-rest
+	                       (list-set exps
+	                                 pos
+	                                 `(var-exp ,var)))))))))))
+    (cps-of-rest exps)))
+
+;; inp-exp-simple? : Inp-exp -> Bool
+(define (inp-exp-simple? exp)
+  (pmatch exp
+    ((const-exp ?) #t)
+    ((var-exp ?) #t)
+    ((diff-exp ,exp1 ,exp2)
+     (and (inp-exp-simple? exp1) (inp-exp-simple? exp2)))
+    ((zero?-exp ,exp1) (inp-exp-simple? exp1))
+    ((proc-exp ? ?) #t)
+    ((sum-exp ,exps) (every inp-exp-simple? exps))
+    (? #f)))
+
+;; make-send-to-cont : Simple-exp x Simple-exp -> Tf-exp
+(define (make-send-to-cont k-exp simple)
+  `(cps-call-exp ,k-exp ,(list simple)))
+
+;; cps-of-sum-exp : List-of(Inp-exp) x Simple-exp -> Tf-exp
+(define (cps-of-sum-exp exps k-exp)
+  (cps-of-exps
+   exps
+   (lambda (sms)
+     (make-send-to-cont k-exp `(cps-sum-exp ,sms)))))
+
+;; cps-of-diff-exp : Inp-exp x Inp-exp x Simple-exp -> Tf-exp
+(define (cps-of-diff-exp exp1 exp2 k-exp)
+  (cps-of-exps
+   (list exp1 exp2)
+   (lambda (sms)
+     (pmatch sms
+       ((,s1 ,s2)
+        (make-send-to-cont k-exp `(cps-diff-exp ,s1 ,s2)))))))
+
+;; cps-of-if-exp : Inp-exp x Inp-exp x Inp-exp x Simple-exp -> Tf-exp
+(define (cps-of-if-exp exp1 exp2 exp3 k-exp)
+  (cps-of-exps
+   (list exp1)
+   (lambda (sms)
+     (make-send-to-cont
+      k-exp
+      `(cps-if-exp ,(car sms)
+                   ,(cps-of-exp exp2 k-exp)
+                   ,(cps-of-exp exp3 k-exp))))))
+
+;; cps-of-let-exp : Var x Inp-exp x Inp-exp x Simple-exp -> Tf-exp
+(define (cps-of-let-exp id rhs body k-exp)
+  (cps-of-exps
+   (list rhs)
+   (lambda (sms)
+     `(cps-let-exp ,id ,(car sms) ,(cps-of-exp body k-exp)))))
+
+;; cps-of-letrec-exp : List-of(Var) x List-of(List-of(Var)) x
+;;                       List-of(Inp-exp) x Simple-exp -> Tf-exp
+(define (cps-of-letrec-exp p-names b-varss p-bodies lr-body k-exp)
+  `(cps-letrec-exp
+    ,p-names
+    ,(map (lambda (b-vars)
+            (append b-vars (list 'k%00)))
+          b-varss)
+    ,(map (lambda (p-body)
+            `(cps-of-exp ,p-body (cps-var-exp k%00)))
+          p-bodies)
+    ,(cps-of-exp lr-body k-exp)))
+
+;; cps-of-simple-exp : Simple-inp-exp -> Simple-exp
+(define (cps-of-simple-exp exp)
+  (pmatch exp
+    ((const-exp ,num) `(cps-const-exp ,num))
+    ((var-exp ,var) `(cps-var-exp ,var))
+    ((diff-exp ,exp1 ,exp2)
+     `(cps-diff-exp ,(cps-of-simple-exp exp1)
+                    ,(cps-of-simple-exp exp2)))
+    ((zero?-exp ,exp1)
+     `(cps-zero?-exp ,(cps-of-simple-exp exp1)))
+    ((proc-exp ,vars ,body)
+     `(cps-proc-exp ,(append vars (list 'k%00))
+                    ,(cps-of-exp body '(cps-var-exp k%00))))
+    ((sum-exp ,exps)
+     `(cps-sum-exp ,(map cps-of-simple-exp exps)))
+    (? (error 'cps-of-simple-exp "invalid simple exp" exp))))
+
+;; cps-of-call-exp : Inp-exp x List-of(Inp-exp) x Simple-exp -> Tf-exp
+(define (cps-of-call-exp rator rands k-exp)
+  (cps-of-exps
+   (cons rator rands)
+   (lambda (sms)
+     `(cps-call-exp ,(car sms)
+     		    ,(append (cdr sms) (list k-exp))))))
+
+;; cps-of-zero?-exp : Inp-exp x Simple-exp -> Tf-exp
+(define (cps-of-zero?-exp exp1 k-exp)
+  (cps-of-exps (list exp1)
+               (lambda (sms)
+                 (make-send-to-cont
+                  k-exp
+                  `(cps-zero?-exp ,(car sms))))))
+
+;;; Main translator dispatch
+
+;; cps-of-exp : Inp-exp x Simple-exp -> Tf-exp
+(define (cps-of-exp exp k-exp)
+  (pmatch exp
+    ((const-exp ,num)
+     (make-send-to-cont k-exp `(cps-const-exp ,num)))
+    ((var-exp ,var)
+     (make-send-to-cont k-exp `(cps-var-exp ,var)))
+    ((proc-exp ,vars ,body)
+     (make-send-to-cont
+      k-exp
+      `(cps-proc-exp (append vars (list 'k%00))
+                     (cps-of-exp body '(cps-var-exp k%00)))))
+    ((zero?-exp ,exp1) (cps-of-zero?-exp exp1 k-exp))
+    ((diff-exp ,exp1 ,exp2) (cps-of-diff-exp exp1 exp2 k-exp))
+    ((sum-exp ,exps) (cps-of-sum-exp exps k-exp))
+    ((if-exp ,exp1 ,exp2 ,exp3) (cps-of-if-exp exp1 exp2 exp3 k-exp))
+    ((let-exp ,var ,exp1 ,body) (cps-of-let-exp var exp1 body k-exp))
+    ((letrec-exp ,nms ,vars ,pbs ,lr-body)
+     (cps-of-letrec-exp nms vars pbs lr-body k-exp))
+    ((call-exp ,rator ,rands) (cps-of-call-exp rator rands k-exp))
+    (? (error 'cps-of-exp "invalid expression" exp))))
