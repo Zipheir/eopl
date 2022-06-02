3979d62d35583ce322ec53cd750d833594b36d8f
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-06-02 17:35:36 -0400

    OO interpreter, initial work.

---
+++ 2e/ch_05/src/objects.scm
@@ -0,0 +1,45 @@
+;;;; Object-oriented language from Ch. 5 (2e)
+
+(import (rnrs base (6))
+        )
+
+(include "../../src/pmatch.scm")
+
+;;;; Stores
+
+;; Contains the current state of the store.
+(define the-store 'uninitialized)
+
+;; empty-store : () -> Sto
+(define (empty-store) '())
+
+;; initialize-store! : () -> Unspecified
+(define (initialize-store!)
+  (set! the-store (empty-store)))
+
+;; reference? : Scheme-val -> Bool
+(define (reference? x)
+  (integer? x))
+
+;; newref : Exp-val -> Ref
+(define (newref val)
+  (let ((next-ref (length the-store)))
+    (set! the-store (append the-store (list val)))
+    next-ref))
+
+;; deref : Ref -> Exp-val
+(define (deref ref)
+  (list-ref the-store ref))
+
+;; setref! : Ref x Exp-val -> Unspecified
+(define (setref! ref val)
+  (letrec
+    ((setref-inner
+      (lambda (store1 ref1)
+        (cond ((null? store1)
+               (report-invalid-reference ref the-store))
+              ((zero? ref1) (cons val (cdr store1)))
+              (else (cons (car store1)
+                          (setref-inner (cdr store1)
+                                        (- ref1 1))))))))
+    (set! the-store (setref-inner the-store ref))))
