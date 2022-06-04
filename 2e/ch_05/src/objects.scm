df0de4f74130747a5277e472e7fd78f9a6107c7a
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-06-04 02:41:24 -0400

    More work on OO interpreter.

--- 2e/ch_05/src/objects.scm
+++ 2e/ch_05/src/objects.scm
@@ -3,43 +3,89 @@
 (import (rnrs base (6))
         )
 
-(include "../../src/pmatch.scm")
+(include "../../../src/pmatch.scm")
 
-;;;; Stores
+;;;; References
 
-;; Contains the current state of the store.
-(define the-store 'uninitialized)
-
-;; empty-store : () -> Sto
-(define (empty-store) '())
-
-;; initialize-store! : () -> Unspecified
-(define (initialize-store!)
-  (set! the-store (empty-store)))
-
-;; reference? : Scheme-val -> Bool
-(define (reference? x)
-  (integer? x))
-
-;; newref : Exp-val -> Ref
-(define (newref val)
-  (let ((next-ref (length the-store)))
-    (set! the-store (append the-store (list val)))
-    next-ref))
-
-;; deref : Ref -> Exp-val
 (define (deref ref)
-  (list-ref the-store ref))
+  (pmatch ref
+    ((a-ref ,k ,vec) (vector-ref vec k))
+    (? (error 'deref "not a reference" ref))))
 
-;; setref! : Ref x Exp-val -> Unspecified
 (define (setref! ref val)
+  (pmatch ref
+    ((a-ref ,k ,vec) (vector-set! vec k val))
+    (? (error 'setref! "not a reference" ref))))
+
+;;;; Expressed values
+
+;; expval->num : Exp-val -> Int
+(define (expval->num val)
+  (pmatch val
+    ((num-val ,n) n)
+    (? (report-expval-extractor-error 'num val))))
+
+;; expval->proc : Exp-val -> Proc
+(define (expval->proc val)
+  (pmatch val
+    ((proc-val ,p) p)
+    (? (report-expval-extractor-error 'proc val))))
+
+(define (report-expval-extractor-error variant value)
+  (error 'expval-extractors
+         "unexpected type found"
+         variant value))
+
+;;;; Procedures
+
+(define (procedure b-var body saved-env)
+  (list 'proc b-var body saved-env))
+
+;;;; Environments
+
+;; empty-env : () -> Env
+(define (empty-env) '())
+
+;; extend-env : List-of(Var) x List-of(Exp-val) x Env -> Env
+(define (extend-env vars vals env)
+  (cons (list 'ext vars (list->vector vals)) env))
+
+;; extend-env-rec : List-of(Var) x List-of(List-of(Var)) x
+;;                    List-of(Exp-val) x Env -> Env
+(define (extend-env-rec p-names b-varss p-bodies old-env)
+  (cons (list 'ext-rec p-names b-varss p-bodies) old-env))
+
+;; apply-env : Env x Var -> Scheme-val
+(define (apply-env env search-var)
+  (deref (apply-env-ref env search-var)))
+
+;; apply-env-ref : Env x Var -> Ref
+(define (apply-env-ref env search-var)
+  (pmatch env
+    (() (report-no-binding-found search-var))
+    (((ext ,vars ,vals) . ,env*)
+     (cond ((rib-find-position search-var vars) =>
+            (lambda (pos) `(a-ref ,pos ,vals)))
+           (else (apply-env-ref env* search-var))))
+    (? (error 'apply-env-ref "invalid environment" env))))
+
+(define (report-no-binding-found var)
+  (error 'apply-env "no binding found" var))
+
+;; rib-find-position : Var x List-of(Var) -> Int-or-false
+(define (rib-find-position var lis)
   (letrec
-    ((setref-inner
-      (lambda (store1 ref1)
-        (cond ((null? store1)
-               (report-invalid-reference ref the-store))
-              ((zero? ref1) (cons val (cdr store1)))
-              (else (cons (car store1)
-                          (setref-inner (cdr store1)
-                                        (- ref1 1))))))))
-    (set! the-store (setref-inner the-store ref))))
+   ((find
+     (lambda (lis i)
+       (cond ((null? lis) #f)
+             ((eqv? (car lis) var) i)
+             (else (find (cdr lis) (+ i 1)))))))
+    (find lis 0)))
+
+;; alist->env : List-of(Var x Scheme-val) -> Env
+;; No recursive bindings.
+(define (alist->env as)
+  (fold-right (lambda (p env)
+                (extend-env (car p) (cdr p) env))
+              (empty-env)
+              as))
